d688aa8a7ffc8e455de9447064aa4a2b090dca29
mahbub.junaidi <mahbub.junaidi@ptap.co.id>
2024-10-17 21:23:18 +0700

    first commit

---
+++ src/test/java/com/ManuLifeApplication/ManuLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ManuLifeApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
